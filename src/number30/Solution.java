0b6d78de61e8f9032c1b07d3379167b922829a96
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-08-02 23:01:11 +0800

    No.30 solved(but too slow!!!).

--- src/number30/Solution.java
+++ src/number30/Solution.java
@@ -16,56 +16,24 @@ public class Solution {
 
         int num = words.length;
         int len = words[0].length();
-//        ArrayList<Character> starts = new ArrayList<>(num);
         ArrayList<String> wordsStore = new ArrayList<>(num);
-        int[] tmpStore = new int[num];
         boolean flag;
 
         Collections.addAll(wordsStore, words);
-//        for (String word : words) {
-//            starts.add(word.charAt(0));
-//            wordsStore.add(word);
-//        }
 
         for (int i = 0; i <= s.length() - (num * len); i++) {
             //重置
             flag = false;
             for (int j = 0; j < num; j++) {
-//                starts.set(j, words[j].charAt(0));
                 wordsStore.set(j, words[j]);
-                tmpStore[j] = 0;
             }
-//            //首先查看首字母是否匹配
-//            for (int k = 0; k < num; k++) {
-//                int index = starts.indexOf(s.charAt(i + k * len));
-//                if (index == -1 || tmpStore[index] > 0) {
-//                    break;
-//                }
-//                tmpStore[index]++;
-//                //防止有首字母重复的
-//                starts.set(index, '#');
-//                if (k == num - 1) flag = true;
-//            }
-//            //如果首字母匹配，则进行字符串比较
-//            if (flag) {
-//                for (int k = 0; k < num; k++) {
-//                    String substr = s.substring(i + k * len, i + (k + 1) * len - 1);
-//                    int index = wordsStore.indexOf(substr);
-//                    if (index == -1 || tmpStore[index] > 0) {
-//                        break;
-//                    }
-//                    tmpStore[index]++;
-//                    wordsStore.set(index, "#");
-//                    if (k == num - 1) flag = true;
-//                }
-//            }
+
             for (int k = 0; k < num; k++) {
-                String substr = s.substring(i + k * len, i + (k + 1) * len - 1);
+                String substr = s.substring(i + k * len, i + (k + 1) * len);
                 int index = wordsStore.indexOf(substr);
                 if (index == -1) {
                     break;
                 }
-                tmpStore[index]++;
                 wordsStore.set(index, "#");
                 if (k == num - 1) flag = true;
             }
