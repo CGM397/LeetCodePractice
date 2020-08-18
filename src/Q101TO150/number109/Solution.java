7b29d1526b9b6aaa939042353cdabd6c083565ac
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-08-18 20:31:22 +0800

    No.109: add a nice solution from someone.

--- src/Q101TO150/number109/Solution.java
+++ src/Q101TO150/number109/Solution.java
@@ -88,6 +88,29 @@ public class Solution {
         }
     }
 
+    public TreeNode  niceSolution(ListNode head) {
+        if(head == null) return null;
+        else if(head.next == null) return new TreeNode(head.val);
+
+        ListNode pre = head;
+        ListNode p = pre.next;
+        ListNode q = p.next;
+        //找到链表的中点p，快慢指针
+        while(q != null && q.next != null){
+            pre = pre.next;
+            p = pre.next;
+            q = q.next.next;
+        }
+        //将中点左边的链表分开
+        pre.next = null;
+
+        TreeNode root = new TreeNode(p.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(p.next);
+
+        return root;
+    }
+
     public static class ListNode {
         int val;
         ListNode next;
