f5fae6e6de49a232f5d0e97b53addde58dfc9803
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-08-29 22:46:34 +0800

    No.55 solved.

---
+++ src/Q51TO100/number55/Solution.java
@@ -0,0 +1,27 @@
+package Q51TO100.number55;
+
+/**
+ * @author: Bright Chan
+ * @date: 2020/8/29 16:50
+ * @description: 回溯算法
+ */
+public class Solution {
+
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) return true;
+        boolean flag;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0 && i < nums.length - 1) {
+                flag = false;
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] > i - j) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag) return false;
+            }
+        }
+        return true;
+    }
+}
