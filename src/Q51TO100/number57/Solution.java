b3c023fa53e92afb17999baaf8ac7285a044cafc
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-09-04 22:22:42 +0800

    No.57 solved.

--- src/Q51TO100/number57/Solution.java
+++ src/Q51TO100/number57/Solution.java
@@ -7,6 +7,43 @@ package Q51TO100.number57;
  */
 public class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        return null;
+        int[][] store = new int[intervals.length + 1][2];
+        int count = 0;
+        boolean flag = false;
+        for (int[] interval : intervals) {
+            if (!flag && newInterval[0] <= interval[0]) {
+                store[count] = newInterval;
+                count++;
+                store[count] = interval;
+                count++;
+                flag = true;
+                continue;
+            }
+            store[count] = interval;
+            count++;
+        }
+        if (!flag) store[count] = newInterval;
+
+        int max = store[0][0] - 1, pos = -1;
+        int[][] tmp = new int[intervals.length + 1][2];
+        for (int[] interval : store) {
+            if (interval[0] > max) {
+                pos++;
+                tmp[pos][0] = interval[0];
+                tmp[pos][1] = interval[1];
+                max = interval[1];
+            }
+            else {
+                max = Math.max(tmp[pos][1], interval[1]);
+                tmp[pos][1] = max;
+            }
+        }
+
+        int[][] res = new int[pos + 1][2];
+        for (int i = 0; i < pos + 1; i++) {
+            res[i][0] = tmp[i][0];
+            res[i][1] = tmp[i][1];
+        }
+        return res;
     }
 }
