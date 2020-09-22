dd5f2deab22b6bb146f95a79860c80beecb07ab4
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-09-22 19:46:22 +0800

    No.968 solved by official solution...

--- src/Q501TO550/number538/Solution.java
+++ src/Q501TO550/number538/Solution.java
@@ -3,7 +3,7 @@ package Q501TO550.number538;
 /**
  * @author: Bright Chan
  * @date: 2020/9/21 21:00
- * @description: 反向中序遍历：右，根，左
+ * @description: 反向中序遍历：右，根，左，可得到递减序列
  */
 public class Solution {
     int sum = 0;
---
+++ src/Q951TO1000/number968/Solution.java
@@ -0,0 +1,34 @@
+package Q951TO1000.number968;
+
+/**
+ * @author: Bright Chan
+ * @date: 2020/9/22 19:31
+ * @description: 递归
+ */
+public class Solution {
+    public int minCameraCover(TreeNode root) {
+        int[] array = dfs(root);
+        return array[1];
+    }
+
+    public int[] dfs(TreeNode root) {
+        if (root == null) {
+            return new int[]{Integer.MAX_VALUE / 2, 0, 0};
+        }
+        int[] leftArray = dfs(root.left);
+        int[] rightArray = dfs(root.right);
+        int[] array = new int[3];
+        array[0] = leftArray[2] + rightArray[2] + 1;
+        array[1] = Math.min(array[0], Math.min(leftArray[0] + rightArray[1], rightArray[0] + leftArray[1]));
+        array[2] = Math.min(array[0], leftArray[1] + rightArray[1]);
+        return array;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
