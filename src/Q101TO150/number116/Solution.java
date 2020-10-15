7cb44f6c584c018499e83c6b31cfb9baee4fdb84
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-10-15 22:41:18 +0800

    No.116 solved by official solution.

---
+++ src/Q101TO150/number116/Solution.java
@@ -0,0 +1,62 @@
+package Q101TO150.number116;
+
+/**
+ * @author: Bright Chan
+ * @date: 2020/10/15 22:37
+ * @description: 广搜；深搜；和No.117相似
+ */
+public class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+
+        // 从根节点开始
+        Node leftmost = root;
+
+        while (leftmost.left != null) {
+
+            // 遍历这一层节点组织成的链表，为下一层的节点更新 next 指针
+            Node head = leftmost;
+
+            while (head != null) {
+
+                // CONNECTION 1
+                head.left.next = head.right;
+
+                // CONNECTION 2
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+
+                // 指针向后移动
+                head = head.next;
+            }
+
+            // 去下一层的最左的节点
+            leftmost = leftmost.left;
+        }
+
+        return root;
+    }
+
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    };
+}
