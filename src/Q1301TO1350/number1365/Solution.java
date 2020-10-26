bbf4a702d869d25b8812ca302da495bc4ba01c34
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-10-26 09:52:39 +0800

    No.1365 solved.

---
+++ src/Q1301TO1350/number1365/Solution.java
@@ -0,0 +1,28 @@
+package Q1301TO1350.number1365;
+
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: Bright Chan
+ * @date: 2020/10/26 9:19
+ * @description: 排序
+ */
+public class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] tmp = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(tmp);
+        List<Integer> store = new ArrayList<>();
+        for (int i : tmp) store.add(i);
+
+
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = store.indexOf(nums[i]);
+        }
+        return res;
+    }
+}
