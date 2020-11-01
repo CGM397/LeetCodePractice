04103086fb3fd4bbd51af377f59fe9a4b782457f
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-11-01 13:21:53 +0800

    No.140 solved by official solution :)

---
+++ src/Q101TO150/number140/Solution.java
@@ -0,0 +1,45 @@
+package Q101TO150.number140;
+
+import java.util.*;
+
+/**
+ * @author: Bright Chan
+ * @date: 2020/11/1 11:09
+ * @description: 记忆化搜索
+ */
+public class Solution {
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        // 使用哈希表存储字符串s的每个下标和从该下标开始的部分可以组成的句子列表
+        Map<Integer, List<List<String>>> map = new HashMap<>();
+        List<List<String>> wordBreaks = backtrack(s, s.length(), new HashSet<>(wordDict), 0, map);
+        List<String> breakList = new LinkedList<>();
+        for (List<String> wordBreak : wordBreaks) {
+            breakList.add(String.join(" ", wordBreak));
+        }
+        return breakList;
+    }
+
+    public List<List<String>> backtrack(String s, int length, Set<String> wordSet,
+                                        int index, Map<Integer, List<List<String>>> map) {
+        if (!map.containsKey(index)) {
+            List<List<String>> wordBreaks = new LinkedList<>();
+            if (index == length) {
+                wordBreaks.add(new LinkedList<>());
+            }
+            for (int i = index + 1; i <= length; i++) {
+                String word = s.substring(index, i);
+                if (wordSet.contains(word)) {
+                    List<List<String>> nextWordBreaks = backtrack(s, length, wordSet, i, map);
+                    for (List<String> nextWordBreak : nextWordBreaks) {
+                        LinkedList<String> wordBreak = new LinkedList<>(nextWordBreak);
+                        wordBreak.offerFirst(word);
+                        wordBreaks.add(wordBreak);
+                    }
+                }
+            }
+            map.put(index, wordBreaks);
+        }
+        return map.get(index);
+    }
+}
