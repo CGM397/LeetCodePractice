43979610918c7878fcf10746e96b7af301abf2d5
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-11-05 11:56:28 +0800

    No.83 solved

--- src/Q51TO100/number83/Solution.java
+++ src/Q51TO100/number83/Solution.java
@@ -1,34 +1,31 @@
-package Q51TO100.number82;
+package Q51TO100.number83;
 
 /**
  * @author: Bright Chan
- * @date: 2020/11/5 11:39
- * @description: 链表，多指针
+ * @date: 2020/11/5 11:53
+ * @description: 链表，双指针
  */
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) return head;
 
-        ListNode pre = new ListNode(-1);
-        ListNode res = pre;
-        pre.next = head;
+
         ListNode cur = head, next = head.next;
 
         while (next != null) {
             if (cur.val == next.val) {
                 while (next != null && next.val == cur.val) next = next.next;
-                pre.next = next;
+                cur.next = next;
                 cur = next;
                 if (next != null) next = next.next;
             }
             else {
-                pre = cur;
                 cur = next;
                 next = next.next;
             }
         }
 
-        return res.next;
+        return head;
     }
 
     public static class ListNode {
