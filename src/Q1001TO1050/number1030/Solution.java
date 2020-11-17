9c36dbfeeb80845caf0dc142d809132061a199c8
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-11-17 14:06:18 +0800

    No.1030 solved

---
+++ src/Q1001TO1050/number1030/Solution.java
@@ -0,0 +1,38 @@
+package Q1001TO1050.number1030;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: Bright Chan
+ * @date: 2020/11/17 13:35
+ * @description: 桶排
+ */
+public class Solution {
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int maxDist = Math.max(r0, R - 1 - r0) + Math.max(c0, C - 1 - c0);
+        List<List<int[]>> bucket = new ArrayList<>();
+        for (int i = 0; i <= maxDist; i++) {
+            bucket.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                int d = dist(i, j, r0, c0);
+                bucket.get(d).add(new int[]{i, j});
+            }
+        }
+        int[][] ret = new int[R * C][];
+        int index = 0;
+        for (int i = 0; i <= maxDist; i++) {
+            for (int[] it : bucket.get(i)) {
+                ret[index++] = it;
+            }
+        }
+        return ret;
+    }
+
+    public int dist(int r1, int c1, int r2, int c2) {
+        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
+    }
+}
