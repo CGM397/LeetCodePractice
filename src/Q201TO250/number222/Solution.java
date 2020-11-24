cf6e565b8350e115fad8408aa433bab11d31380c
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-11-24 21:31:48 +0800

    No.222 solved by official solution.

---
+++ src/Q201TO250/number222/Solution.java
@@ -0,0 +1,51 @@
+package Q201TO250.number222;
+
+/**
+ * @author: Bright Chan
+ * @date: 2020/11/24 21:28
+ * @description: 二分查找+位运算
+ */
+public class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
