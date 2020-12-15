d60d9fdd02c03bfb155efdb716630d0ee106a143
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-12-15 13:17:42 +0800

    No.738 solved.

---
+++ src/Q701TO750/number738/Solution.java
@@ -0,0 +1,26 @@
+package Q701TO750.number738;
+
+/**
+ * @author: Bright Chan
+ * @date: 2020/12/15 12:40
+ * @description: 贪心
+ */
+public class Solution {
+    public int monotoneIncreasingDigits(int N) {
+        char[] strN = Integer.toString(N).toCharArray();
+        int i = 1;
+        while (i < strN.length && strN[i - 1] <= strN[i]) {
+            i += 1;
+        }
+        if (i < strN.length) {
+            while (i > 0 && strN[i - 1] > strN[i]) {
+                strN[i - 1] -= 1;
+                i -= 1;
+            }
+            for (i += 1; i < strN.length; ++i) {
+                strN[i] = '9';
+            }
+        }
+        return Integer.parseInt(new String(strN));
+    }
+}
