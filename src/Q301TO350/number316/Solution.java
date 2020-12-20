701c6d0712e94dff48f14adc54ec61c58efa7a29
Bright Chan <33258775+CGM397@users.noreply.github.com>
2020-12-20 21:31:07 +0800

    No.316 solved by official solution.

---
+++ src/Q301TO350/number316/Solution.java
@@ -0,0 +1,35 @@
+package Q301TO350.number316;
+
+/**
+ * @author: Bright Chan
+ * @date: 2020/12/20 20:41
+ * @description: 栈，贪心
+ */
+public class Solution {
+    public String removeDuplicateLetters(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
+}
