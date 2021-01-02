403119af2852e6d82b0b23ca2422905c757335a7
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-01-02 11:28:37 +0800

    learn the comparator.

--- src/Q1001TO1050/number1046/Solution.java
+++ src/Q1001TO1050/number1046/Solution.java
@@ -1,6 +1,5 @@
 package Q1001TO1050.number1046;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -21,5 +20,9 @@ public class Solution {
         }
         return store.isEmpty() ? 0 : store.poll();
     }
+
+    // return 负数 表示第一个参数应该排在第二个参数之前；
+    // return 正数 表示第一个参数应该排在第二个参数之后；
+    // return 零   表示两数相等。
     private static Comparator<Integer> cmp = (e1, e2) -> e2 - e1;
 }
