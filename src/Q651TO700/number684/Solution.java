8cf391d429ff17a76aff1e2ad63fdde019839324
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-01-13 11:39:31 +0800

    No.684 solved by official solution.

---
+++ src/Q651TO700/number684/Solution.java
@@ -0,0 +1,36 @@
+package Q651TO700.number684;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/1/13 11:31
+ * @description: 并查集
+ */
+public class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int nodesCount = edges.length;
+        int[] parent = new int[nodesCount + 1];
+        for (int i = 1; i <= nodesCount; i++) {
+            parent[i] = i;
+        }
+        for (int[] edge : edges) {
+            int node1 = edge[0], node2 = edge[1];
+            if (find(parent, node1) != find(parent, node2)) {
+                union(parent, node1, node2);
+            } else {
+                return edge;
+            }
+        }
+        return new int[0];
+    }
+
+    public void union(int[] parent, int index1, int index2) {
+        parent[find(parent, index1)] = find(parent, index2);
+    }
+
+    public int find(int[] parent, int index) {
+        if (parent[index] != index) {
+            parent[index] = find(parent, parent[index]);
+        }
+        return parent[index];
+    }
+}
