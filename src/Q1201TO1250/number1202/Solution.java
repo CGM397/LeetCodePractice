a1e8b256b11749bece8ce10e4b4bde8cf2b5c5d1
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-01-24 13:13:23 +0800

    No.547 1202 solved again with union-find set.

--- src/Q1201TO1250/number1202/Solution.java
+++ src/Q1201TO1250/number1202/Solution.java
@@ -1,9 +1,6 @@
 package Q1201TO1250.number1202;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * @author: Bright Chan
@@ -11,88 +8,166 @@ import java.util.PriorityQueue;
  * @description: 并查集
  */
 public class Solution {
+
+    // my solution
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-        if (pairs.size() == 0) {
-            return s;
-        }
-
-        // 第 1 步：将任意交换的结点对输入并查集
+        char[] store = s.toCharArray();
         int len = s.length();
-        UnionFind unionFind = new UnionFind(len);
-        for (List<Integer> pair : pairs) {
-            int index1 = pair.get(0);
-            int index2 = pair.get(1);
-            unionFind.union(index1, index2);
+        int[] parents = new int[len];
+        int[] ranks = new int[len];
+        for (int i = 0; i < len; i++) {
+            parents[i] = i;
+            ranks[i] = 0;
         }
 
-        // 第 2 步：构建映射关系
-        char[] charArray = s.toCharArray();
-        // key：连通分量的代表元，value：同一个连通分量的字符集合（保存在一个优先队列中）
-        Map<Integer, PriorityQueue<Character>> hashMap = new HashMap<>(len);
-        for (int i = 0; i < len; i++) {
-            int root = unionFind.find(i);
-            if (hashMap.containsKey(root)) {
-                hashMap.get(root).offer(charArray[i]);
-            } else {
-                // PriorityQueue<Character> minHeap = new PriorityQueue<>();
-                // minHeap.offer(charArray[i]);
-                // hashMap.put(root, minHeap);
-                // 上面三行代码等价于下面一行代码，JDK 1.8 以及以后支持下面的写法
-                hashMap.computeIfAbsent(root, key -> new PriorityQueue<>()).offer(charArray[i]);
+        for (List<Integer> one : pairs) {
+            int parent1 = findParent(parents, one.get(0));
+            int parent2 = findParent(parents, one.get(1));
+
+            if (parent1 != parent2) {
+                union(parents, ranks, parent1, parent2);
             }
         }
 
-        // 第 3 步：重组字符串
-        StringBuilder stringBuilder = new StringBuilder();
+        Map<Integer, List<Character>> storeChar = new HashMap<>();
+        Map<Integer, List<Integer>> storeIndex = new HashMap<>();
         for (int i = 0; i < len; i++) {
-            int root = unionFind.find(i);
-            stringBuilder.append(hashMap.get(root).poll());
+            int parent = findParent(parents, i);
+            List<Character> chars = storeChar.getOrDefault(parent, new ArrayList<>());
+            chars.add(store[i]);
+            storeChar.put(parent, chars);
+
+            List<Integer> indexs = storeIndex.getOrDefault(parent, new ArrayList<>());
+            indexs.add(i);
+            storeIndex.put(parent, indexs);
         }
-        return stringBuilder.toString();
+        for (Integer index : storeChar.keySet()) {
+            Collections.sort(storeChar.get(index));
+        }
+
+        char[] res = new char[len];
+
+        for (Integer index : storeChar.keySet()) {
+            List<Character> chars = storeChar.get(index);
+            List<Integer> indexs = storeIndex.get(index);
+
+            for (int i = 0; i < chars.size(); i++) {
+                res[indexs.get(i)] = chars.get(i);
+            }
+        }
+        return String.valueOf(res);
     }
 
-    private static class UnionFind {
-
-        private int[] parent;
-        /**
-         * 以 i 为根结点的子树的高度（引入了路径压缩以后该定义并不准确）
-         */
-        private int[] rank;
-
-        public UnionFind(int n) {
-            this.parent = new int[n];
-            this.rank = new int[n];
-            for (int i = 0; i < n; i++) {
-                this.parent[i] = i;
-                this.rank[i] = 1;
-            }
+    // 按秩合并
+    public void union(int[] parents, int[] ranks, int parent1, int parent2) {
+        // parent1的高度比parent2的高度小，所以可以将parent1合并到parent2上，这样parent2
+        // 的秩并不会变大
+        if (ranks[parent1] < ranks[parent2]) {
+            parents[parent1] = parent2;
         }
+        else {
+            parents[parent2] = parent1;
 
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX == rootY) {
-                return;
+            // 如果两个节点的高度一样，则合并之后秩肯定会加一
+            // 因为原本作为根节点的节点现在有了一个父节点
+            if (ranks[parent1] == ranks[parent2]) {
+                ranks[parent1]++;
             }
-
-            if (rank[rootX] == rank[rootY]) {
-                parent[rootX] = rootY;
-                // 此时以 rootY 为根结点的树的高度仅加了 1
-                rank[rootY]++;
-            } else if (rank[rootX] < rank[rootY]) {
-                parent[rootX] = rootY;
-                // 此时以 rootY 为根结点的树的高度不变
-            } else {
-                // 同理，此时以 rootX 为根结点的树的高度不变
-                parent[rootY] = rootX;
-            }
-        }
-
-        public int find(int x) {
-            if (x != parent[x]) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
         }
     }
+
+    // 路径压缩优化后的合并操作
+    // 要记得，findParent方法不是找到直接父节点，而是根节点
+    public int findParent(int[] parent, int node) {
+        if (parent[node] != node) {
+            parent[node] = findParent(parent, parent[node]);
+        }
+        return parent[node];
+    }
+
+    // official solution
+//    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+//        if (pairs.size() == 0) {
+//            return s;
+//        }
+//
+//        // 第 1 步：将任意交换的结点对输入并查集
+//        int len = s.length();
+//        UnionFind unionFind = new UnionFind(len);
+//        for (List<Integer> pair : pairs) {
+//            int index1 = pair.get(0);
+//            int index2 = pair.get(1);
+//            unionFind.union(index1, index2);
+//        }
+//
+//        // 第 2 步：构建映射关系
+//        char[] charArray = s.toCharArray();
+//        // key：连通分量的代表元，value：同一个连通分量的字符集合（保存在一个优先队列中）
+//        Map<Integer, PriorityQueue<Character>> hashMap = new HashMap<>(len);
+//        for (int i = 0; i < len; i++) {
+//            int root = unionFind.find(i);
+//            if (hashMap.containsKey(root)) {
+//                hashMap.get(root).offer(charArray[i]);
+//            } else {
+//                // PriorityQueue<Character> minHeap = new PriorityQueue<>();
+//                // minHeap.offer(charArray[i]);
+//                // hashMap.put(root, minHeap);
+//                // 上面三行代码等价于下面一行代码，JDK 1.8 以及以后支持下面的写法
+//                hashMap.computeIfAbsent(root, key -> new PriorityQueue<>()).offer(charArray[i]);
+//            }
+//        }
+//
+//        // 第 3 步：重组字符串
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for (int i = 0; i < len; i++) {
+//            int root = unionFind.find(i);
+//            stringBuilder.append(hashMap.get(root).poll());
+//        }
+//        return stringBuilder.toString();
+//    }
+//
+//    private static class UnionFind {
+//
+//        private int[] parent;
+//        /**
+//         * 以 i 为根结点的子树的高度（引入了路径压缩以后该定义并不准确）
+//         */
+//        private int[] rank;
+//
+//        public UnionFind(int n) {
+//            this.parent = new int[n];
+//            this.rank = new int[n];
+//            for (int i = 0; i < n; i++) {
+//                this.parent[i] = i;
+//                this.rank[i] = 1;
+//            }
+//        }
+//
+//        public void union(int x, int y) {
+//            int rootX = find(x);
+//            int rootY = find(y);
+//            if (rootX == rootY) {
+//                return;
+//            }
+//
+//            if (rank[rootX] == rank[rootY]) {
+//                parent[rootX] = rootY;
+//                // 此时以 rootY 为根结点的树的高度仅加了 1
+//                rank[rootY]++;
+//            } else if (rank[rootX] < rank[rootY]) {
+//                parent[rootX] = rootY;
+//                // 此时以 rootY 为根结点的树的高度不变
+//            } else {
+//                // 同理，此时以 rootX 为根结点的树的高度不变
+//                parent[rootY] = rootX;
+//            }
+//        }
+//
+//        public int find(int x) {
+//            if (x != parent[x]) {
+//                parent[x] = find(parent[x]);
+//            }
+//            return parent[x];
+//        }
+//    }
 }
--- src/Q501TO550/number547/Solution.java
+++ src/Q501TO550/number547/Solution.java
@@ -1,36 +1,97 @@
 package Q501TO550.number547;
 
-import java.util.Arrays;
-
 /**
  * @author: Bright Chan
  * @date: 2021/1/7 11:16
  * @description: 图，dfs
  */
 public class Solution {
+
+    // union-find set solution
     public int findCircleNum(int[][] isConnected) {
         int len = isConnected.length;
-        boolean[] flag = new boolean[len];
-        Arrays.fill(flag, false);
-
-        int res = 0;
+        int[] parents = new int[len];
+        int[] ranks = new int[len];
         for (int i = 0; i < len; i++) {
-            if (!flag[i]) {
-                dfs(i, isConnected, flag);
-                res++;
-            }
+            parents[i] = i;
+            ranks[i] = 0;
         }
 
-        return res;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (isConnected[i][j] == 0) continue;
+
+                int parent1 = findParent(parents, i);
+                int parent2 = findParent(parents, j);
+                if (parent1 != parent2) {
+                    union(parents, ranks, parent1, parent2);
+                }
+            }
+        }
+        return getSetNum(parents);
     }
 
-    private void dfs (int i, int[][] isConnected, boolean[] flag) {
-        flag[i] = true;
+    // 按秩合并
+    private void union(int[] parents, int[] ranks, int parent1, int parent2) {
+        // parent1的高度比parent2的高度小，所以可以将parent1合并到parent2上，这样parent2
+        // 的秩并不会变大
+        if (ranks[parent1] < ranks[parent2]) {
+            parents[parent1] = parent2;
+        }
+        else {
+            parents[parent2] = parent1;
 
-        for (int j = 0; j < isConnected.length; j++) {
-            if (!flag[j] && isConnected[i][j] == 1) {
-                dfs(j, isConnected, flag);
+            // 如果两个节点的高度一样，则合并之后秩肯定会加一
+            // 因为原本作为根节点的节点现在有了一个父节点
+            if (ranks[parent1] == ranks[parent2]) {
+                ranks[parent1]++;
             }
         }
     }
+
+    // 路径压缩优化后的合并操作
+    // 要记得，findParent方法不是找到直接父节点，而是根节点
+    private int findParent(int[] parent, int node) {
+        if (parent[node] != node) {
+            parent[node] = findParent(parent, parent[node]);
+        }
+        return parent[node];
+    }
+
+    // 得到集合数
+    private int getSetNum(int[] parents) {
+        int count = 0;
+
+        for (int i = 0; i < parents.length; i++) {
+            if (parents[i] == i) count++;
+        }
+        return count;
+    }
+
+    // dfs solution
+//    public int findCircleNum(int[][] isConnected) {
+//        int len = isConnected.length;
+//        boolean[] flag = new boolean[len];
+//        Arrays.fill(flag, false);
+//
+//        int res = 0;
+//        for (int i = 0; i < len; i++) {
+//            if (!flag[i]) {
+//                dfs(i, isConnected, flag);
+//                res++;
+//            }
+//        }
+//
+//        return res;
+//    }
+//
+//    private void dfs (int i, int[][] isConnected, boolean[] flag) {
+//        flag[i] = true;
+//
+//        for (int j = 0; j < isConnected.length; j++) {
+//            if (!flag[j] && isConnected[i][j] == 1) {
+//                dfs(j, isConnected, flag);
+//            }
+//        }
+//    }
 }
