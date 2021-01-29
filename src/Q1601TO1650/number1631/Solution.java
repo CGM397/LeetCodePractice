807f30be828268bdf060ddc7983850fef791cd31
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-01-29 10:41:08 +0800

    No.1631 solved by official solution.

---
+++ src/Q1601TO1650/number1631/Solution.java
@@ -0,0 +1,46 @@
+package Q1601TO1650.number1631;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/1/29 10:24
+ * @description: 二分查找；
+ */
+public class Solution {
+    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    // 最大值的最小值一般可以使用二分查找，以带求解的值为变量，进行二分查找
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        int left = 0, right = 999999, ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(new int[]{0, 0});
+            boolean[] seen = new boolean[m * n];
+            seen[0] = true;
+            while (!queue.isEmpty()) {
+                int[] cell = queue.poll();
+                int x = cell[0], y = cell[1];
+                for (int i = 0; i < 4; ++i) {
+                    int nx = x + dirs[i][0];
+                    int ny = y + dirs[i][1];
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && !seen[nx * n + ny] && Math.abs(heights[x][y] - heights[nx][ny]) <= mid) {
+                        queue.offer(new int[]{nx, ny});
+                        seen[nx * n + ny] = true;
+                    }
+                }
+            }
+            if (seen[m * n - 1]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
