b13961c990fe850f5bbb2c02ea6d798db71a1b73
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-02-08 18:42:53 +0800

    No.978 solved.

--- src/Q951TO1000/number978/Solution.java
+++ src/Q951TO1000/number978/Solution.java
@@ -17,51 +17,42 @@ public class Solution {
                 if (!firstPattern && !secondPattern) {
                     if ((right & 1) == 1) firstPattern = true;
                     else secondPattern = true;
-                    System.out.println("1");
                 }
                 else if (firstPattern && (right & 1) == 0) {
                     secondPattern = true;
                     firstPattern = false;
                     maxCount = Math.max(right - left, maxCount);
                     left = right - 1;
-                    System.out.println("2");
                 }
                 else if (secondPattern && (right & 1) == 1) {
                     firstPattern = true;
                     secondPattern = false;
                     maxCount = Math.max(right - left, maxCount);
                     left = right - 1;
-                    System.out.println("3");
                 }
-                System.out.println("a");
             }
             else if (arr[right - 1] > arr[right]) {
                 if (!firstPattern && !secondPattern) {
                     if ((right & 1) == 0) firstPattern = true;
                     else secondPattern = true;
-                    System.out.println("4");
                 }
                 else if (firstPattern && (right & 1) == 1) {
                     secondPattern = true;
                     firstPattern = false;
                     maxCount = Math.max(right - left, maxCount);
                     left = right - 1;
-                    System.out.println("5");
                 }
                 else if (secondPattern && (right & 1) == 0) {
                     firstPattern = true;
                     secondPattern = false;
                     maxCount = Math.max(right - left, maxCount);
                     left = right - 1;
-                    System.out.println("6");
                 }
-                System.out.println("a");
             }
             else {
                 maxCount = Math.max(right - left, maxCount);
                 left = right;
                 firstPattern = secondPattern = false;
-                System.out.println("7");
             }
             right++;
         }
