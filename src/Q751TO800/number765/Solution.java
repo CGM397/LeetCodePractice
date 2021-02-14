54a5ac2f5e258d48a236beb1f586db565514f79c
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-02-14 22:17:49 +0800

    No.765 solved official solution.

---
+++ src/Q751TO800/number765/Solution.java
@@ -0,0 +1,53 @@
+package Q751TO800.number765;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/2/14 9:30
+ * @description: 并查集
+ */
+public class Solution {
+    public int minSwapsCouples(int[] row) {
+        int n = row.length;
+        int tot = n / 2;
+        int[] f = new int[tot];
+        for (int i = 0; i < tot; i++) {
+            f[i] = i;
+        }
+
+        for (int i = 0; i < n; i += 2) {
+            int l = row[i] / 2;
+            int r = row[i + 1] / 2;
+            add(f, l, r);
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < tot; i++) {
+            int fx = getf(f, i);
+            map.put(fx, map.getOrDefault(fx, 0) + 1);
+        }
+
+        int ret = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            ret += entry.getValue() - 1;
+        }
+        return ret;
+    }
+
+    public int getf(int[] f, int x) {
+        if (f[x] == x) {
+            return x;
+        }
+        int newf = getf(f, f[x]);
+        f[x] = newf;
+        return newf;
+    }
+
+    public void add(int[] f, int x, int y) {
+        int fx = getf(f, x);
+        int fy = getf(f, y);
+        f[fx] = fy;
+    }
+}
