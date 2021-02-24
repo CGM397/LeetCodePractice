efc34f03e256236e2f236ac3c95cb91f9ea416ce
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-02-24 22:16:21 +0800

    No.832 solved.

---
+++ src/Q801TO850/number832/Solution.java
@@ -0,0 +1,27 @@
+package Q801TO850.number832;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/2/24 22:15
+ * @description: 数组
+ */
+public class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                if (A[i][left] == A[i][right]) {
+                    A[i][left] ^= 1;
+                    A[i][right] ^= 1;
+                }
+                left++;
+                right--;
+            }
+            if (left == right) {
+                A[i][left] ^= 1;
+            }
+        }
+        return A;
+    }
+}
