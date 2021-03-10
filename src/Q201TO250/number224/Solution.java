860d3123a760ef04f073729ed2c7abec2061c5ad
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-03-10 10:03:32 +0800

    No.224 solved.

---
+++ src/Q201TO250/number224/Solution.java
@@ -0,0 +1,46 @@
+package Q201TO250.number224;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/3/10 9:24
+ * @description: 栈
+ */
+public class Solution {
+    public int calculate(String s) {
+        Deque<Integer> ops = new LinkedList<>();
+        ops.push(1);
+        int sign = 1;
+
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '+') {
+                sign = ops.peek();
+                i++;
+            } else if (s.charAt(i) == '-') {
+                sign = -ops.peek();
+                i++;
+            } else if (s.charAt(i) == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                ops.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+        return ret;
+    }
+}
