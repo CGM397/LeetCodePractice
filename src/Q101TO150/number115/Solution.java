75a93746bebcde8ea1a25ea7b1916642a566cfc8
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-03-17 10:43:12 +0800

    No.115 solved by official solution.

---
+++ src/Q101TO150/number115/Solution.java
@@ -0,0 +1,31 @@
+package Q101TO150.number115;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/3/17 10:24
+ * @description: 动态规划
+ */
+public class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        if (m < n) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            char sChar = s.charAt(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char tChar = t.charAt(j);
+                if (sChar == tChar) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
--- src/Q1TO50/number10/Solution.java
+++ src/Q1TO50/number10/Solution.java
@@ -3,11 +3,41 @@ package Q1TO50.number10;
 /**
  * @author: Bright Chan
  * @date: 2021/3/7 15:51
- * @description: TODO
+ * @description: 动态规划
  */
 public class Solution {
     public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
 
-        return false;
+        //  s 的前 i 个字符与 p 中的前 j 个字符是否能够匹配
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                }
+                else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
     }
 }
