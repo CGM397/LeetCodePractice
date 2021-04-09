8c1de6c3c15543c0dd00de851a846e236ff5f950
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-04-09 10:44:02 +0800

    No.154 solved.

---
+++ src/Q151TO200/number154/Solution.java
@@ -0,0 +1,24 @@
+package Q151TO200.number154;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/4/9 10:43
+ * @description: 二分
+ */
+public class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else if (nums[pivot] > nums[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return nums[low];
+    }
+}
