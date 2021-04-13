4fa43ee47b12837544629292fa6ce38a467894ca
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-04-13 13:52:50 +0800

    No.783 solved.

---
+++ src/Q751TO800/number783/Solution.java
@@ -0,0 +1,43 @@
+package Q751TO800.number783;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/4/13 13:08
+ * @description: 二叉树
+ */
+public class Solution {
+    int pre;
+    int ans;
+
+    public int minDiffInBST(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre != -1) {
+            ans = Math.min(ans, root.val - pre);
+        }
+        pre = root.val;
+        dfs(root.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
