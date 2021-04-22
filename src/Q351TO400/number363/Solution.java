9e8e53808d0ccebbfff2066e2bf82dabf8d761cf
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-04-22 12:40:13 +0800

    No.91 363 solved.

---
+++ src/Q351TO400/number363/Solution.java
@@ -0,0 +1,35 @@
+package Q351TO400.number363;
+
+import java.util.TreeSet;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/4/22 12:38
+ * @description: 动态规划
+ */
+public class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int ans = Integer.MIN_VALUE;
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 0; i < m; ++i) { // 枚举上边界
+            int[] sum = new int[n];
+            for (int j = i; j < m; ++j) { // 枚举下边界
+                for (int c = 0; c < n; ++c) {
+                    sum[c] += matrix[j][c]; // 更新每列的元素和
+                }
+                TreeSet<Integer> sumSet = new TreeSet<>();
+                sumSet.add(0);
+                int s = 0;
+                for (int v : sum) {
+                    s += v;
+                    Integer ceil = sumSet.ceiling(s - k);
+                    if (ceil != null) {
+                        ans = Math.max(ans, s - ceil);
+                    }
+                    sumSet.add(s);
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/Q51TO100/number91/Solution.java
@@ -0,0 +1,23 @@
+package Q51TO100.number91;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/4/22 12:37
+ * @description: 动态规划
+ */
+public class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            if (s.charAt(i - 1) != '0') {
+                f[i] += f[i - 1];
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                f[i] += f[i - 2];
+            }
+        }
+        return f[n];
+    }
+}
