0cc613a3875ae9591602cd1bde47a31e14ae0ffe
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-04-29 15:02:11 +0800

    No.403 solved.

---
+++ src/Q401TO450/number403/Solution.java
@@ -0,0 +1,32 @@
+package Q401TO450.number403;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/4/29 15:01
+ * @description: 动态规划
+ */
+public class Solution {
+    public boolean canCross(int[] stones) {
+        int n = stones.length;
+        boolean[][] dp = new boolean[n][n];
+        dp[0][0] = true;
+        for (int i = 1; i < n; ++i) {
+            if (stones[i] - stones[i - 1] > i) {
+                return false;
+            }
+        }
+        for (int i = 1; i < n; ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                int k = stones[i] - stones[j];
+                if (k > j + 1) {
+                    break;
+                }
+                dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+                if (i == n - 1 && dp[i][k]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
