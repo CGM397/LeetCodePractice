b1836a86be38f56871f999500be4104cd517b98c
Bright Chan <33258775+CGM397@users.noreply.github.com>
2021-05-07 09:56:14 +0800

    No.1486 solved.

---
+++ src/Q1451TO1500/number1486/Solution.java
@@ -0,0 +1,16 @@
+package Q1451TO1500.number1486;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/5/7 9:50
+ * @description: 异或
+ */
+public class Solution {
+    public int xorOperation(int n, int start) {
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans ^= (start + i * 2);
+        }
+        return ans;
+    }
+}
