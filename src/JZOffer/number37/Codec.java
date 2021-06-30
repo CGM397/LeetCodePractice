1cdf5eaadd3dfbaf62720e5a77688b088d45987d
Bright Chan <980231201@qq.com>
2021-06-30 20:32:28 +0800

    JZ No.37 solved.

---
+++ src/JZOffer/number37/Codec.java
@@ -0,0 +1,54 @@
+package JZOffer.number37;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/6/30 20:20
+ * @description: 深搜
+ */
+public class Codec {
+    public String serialize(TreeNode root) {
+        return rserialize(root, "");
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] dataArray = data.split(",");
+        List<String> dataList = new LinkedList<>(Arrays.asList(dataArray));
+        return rdeserialize(dataList);
+    }
+
+    public String rserialize(TreeNode root, String str) {
+        if (root == null) {
+            str += "None,";
+        } else {
+            str += str.valueOf(root.val) + ",";
+            str = rserialize(root.left, str);
+            str = rserialize(root.right, str);
+        }
+        return str;
+    }
+
+    public TreeNode rdeserialize(List<String> dataList) {
+        if (dataList.get(0).equals("None")) {
+            dataList.remove(0);
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(dataList.get(0)));
+        dataList.remove(0);
+        root.left = rdeserialize(dataList);
+        root.right = rdeserialize(dataList);
+
+        return root;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
