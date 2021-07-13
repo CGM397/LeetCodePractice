6bf696246c76d50495c71f95442b4469a115bece
Bright Chan <980231201@qq.com>
2021-07-13 21:57:15 +0800

    No.218 solved.

---
+++ src/Q201TO250/number218/Solution.java
@@ -0,0 +1,38 @@
+package Q201TO250.number218;
+
+import java.util.*;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/7/13 21:45
+ * @description: 优先队列
+ */
+public class Solution {
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        List<Integer> boundaries = new ArrayList<>();
+        for (int[] building : buildings) {
+            boundaries.add(building[0]);
+            boundaries.add(building[1]);
+        }
+        Collections.sort(boundaries);
+
+        List<List<Integer>> ret = new ArrayList<>();
+        int n = buildings.length, idx = 0;
+        for (int boundary : boundaries) {
+            while (idx < n && buildings[idx][0] <= boundary) {
+                pq.offer(new int[]{buildings[idx][1], buildings[idx][2]});
+                idx++;
+            }
+            while (!pq.isEmpty() && pq.peek()[0] <= boundary) {
+                pq.poll();
+            }
+
+            int maxn = pq.isEmpty() ? 0 : pq.peek()[1];
+            if (ret.size() == 0 || maxn != ret.get(ret.size() - 1).get(1)) {
+                ret.add(Arrays.asList(boundary, maxn));
+            }
+        }
+        return ret;
+    }
+}
