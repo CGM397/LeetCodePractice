eaba147b0606f50310f869f4f40e5eab7309a7f6
Bright Chan <980231201@qq.com>
2021-07-25 10:31:03 +0800

    No.1743 solved.

---
+++ src/Q1701TO1750/number1743/Solution.java
@@ -0,0 +1,38 @@
+package Q1701TO1750.number1743;
+
+import java.util.*;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/7/25 10:11
+ * @description: 哈希表
+ */
+public class Solution {
+    public int[] restoreArray(int[][] adjacentPairs) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] adjacentPair : adjacentPairs) {
+            map.putIfAbsent(adjacentPair[0], new ArrayList<>());
+            map.putIfAbsent(adjacentPair[1], new ArrayList<>());
+            map.get(adjacentPair[0]).add(adjacentPair[1]);
+            map.get(adjacentPair[1]).add(adjacentPair[0]);
+        }
+
+        int n = adjacentPairs.length + 1;
+        int[] ret = new int[n];
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            int e = entry.getKey();
+            List<Integer> adj = entry.getValue();
+            if (adj.size() == 1) {
+                ret[0] = e;
+                break;
+            }
+        }
+
+        ret[1] = map.get(ret[0]).get(0);
+        for (int i = 2; i < n; i++) {
+            List<Integer> adj = map.get(ret[i - 1]);
+            ret[i] = ret[i - 2] == adj.get(0) ? adj.get(1) : adj.get(0);
+        }
+        return ret;
+    }
+}
