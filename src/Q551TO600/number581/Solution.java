69f67fd06fceedd11b409804057ec52e56ccc61f
Bright Chan <980231201@qq.com>
2021-08-03 21:47:36 +0800

    No.581 solved.

---
+++ src/Q551TO600/number581/Solution.java
@@ -0,0 +1,27 @@
+package Q551TO600.number581;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/8/3 21:01
+ * @description: 排序
+ */
+public class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int maxn = Integer.MIN_VALUE, right = -1;
+        int minn = Integer.MAX_VALUE, left = -1;
+        for (int i = 0; i < n; i++) {
+            if (maxn > nums[i]) {
+                right = i;
+            } else {
+                maxn = nums[i];
+            }
+            if (minn < nums[n - i - 1]) {
+                left = n - i - 1;
+            } else {
+                minn = nums[n - i - 1];
+            }
+        }
+        return right == -1 ? 0 : right - left + 1;
+    }
+}
