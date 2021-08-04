c8410d3fb2996bad377ffa38947fc37adebe3499
Bright Chan <980231201@qq.com>
2021-08-04 21:42:00 +0800

    No.611 solved.

---
+++ src/Q601TO650/number611/Solution.java
@@ -0,0 +1,33 @@
+package Q601TO650.number611;
+
+import java.util.Arrays;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/8/4 21:19
+ * @description: 排序，二分查找
+ */
+public class Solution {
+    public int triangleNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int left = j + 1, right = n - 1, k = j;
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+                    if (nums[mid] < nums[i] + nums[j]) {
+                        k = mid;
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+                ans += k - j;
+            }
+        }
+        return ans;
+    }
+
+}
