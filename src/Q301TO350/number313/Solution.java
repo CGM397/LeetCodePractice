b4439d3f3fa06b40d130e591be3366c8ba7a7696
Bright Chan <980231201@qq.com>
2021-08-09 22:14:37 +0800

    No.313 solved.

---
+++ src/Q301TO350/number313/Solution.java
@@ -0,0 +1,33 @@
+package Q301TO350.number313;
+
+import java.util.Arrays;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/8/9 21:57
+ * @description: dp
+ */
+public class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int m = primes.length;
+        int[] pointers = new int[m];
+        Arrays.fill(pointers, 1);
+        for (int i = 2; i <= n; i++) {
+            int[] nums = new int[m];
+            int minNum = Integer.MAX_VALUE;
+            for (int j = 0; j < m; j++) {
+                nums[j] = dp[pointers[j]] * primes[j];
+                minNum = Math.min(minNum, nums[j]);
+            }
+            dp[i] = minNum;
+            for (int j = 0; j < m; j++) {
+                if (minNum == nums[j]) {
+                    pointers[j]++;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
