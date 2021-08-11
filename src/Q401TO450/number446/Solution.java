213e81da36cfd6f3326edd3e37989372f0f55ae4
Bright Chan <980231201@qq.com>
2021-08-11 22:34:54 +0800

    No.446 solved.

---
+++ src/Q401TO450/number446/Solution.java
@@ -0,0 +1,29 @@
+package Q401TO450.number446;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/8/11 22:34
+ * @description: dp
+ */
+public class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        Map<Long, Integer>[] f = new Map[n];
+        for (int i = 0; i < n; ++i) {
+            f[i] = new HashMap<Long, Integer>();
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                long d = (long) nums[i] - nums[j];
+                int cnt = f[j].getOrDefault(d, 0);
+                ans += cnt;
+                f[i].put(d, f[i].getOrDefault(d, 0) + cnt + 1);
+            }
+        }
+        return ans;
+    }
+}
