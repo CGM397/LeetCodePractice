a41b58560b3fa042af351c1ca15fd9452b71ffdf
Bright Chan <980231201@qq.com>
2021-08-15 10:23:12 +0800

    No.1583 solved.

---
+++ src/Q551TO600/number576/Solution.java
@@ -0,0 +1,34 @@
+package Q551TO600.number576;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/8/15 10:11
+ * @description: dp
+ */
+public class Solution {
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        final int MOD = 1000000007;
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int outCounts = 0;
+        int[][][] dp = new int[maxMove + 1][m][n];
+        dp[0][startRow][startColumn] = 1;
+        for (int i = 0; i < maxMove; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < n; k++) {
+                    int count = dp[i][j][k];
+                    if (count > 0) {
+                        for (int[] direction : directions) {
+                            int j1 = j + direction[0], k1 = k + direction[1];
+                            if (j1 >= 0 && j1 < m && k1 >= 0 && k1 < n) {
+                                dp[i + 1][j1][k1] = (dp[i + 1][j1][k1] + count) % MOD;
+                            } else {
+                                outCounts = (outCounts + count) % MOD;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return outCounts;
+    }
+}
