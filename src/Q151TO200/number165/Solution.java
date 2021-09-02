2d2869a0b149b1bb3251146126e89ff3483c9848
Bright Chan <980231201@qq.com>
2021-09-02 17:25:01 +0800

    No.165 solved.

---
+++ src/Q151TO200/number165/Solution.java
@@ -0,0 +1,29 @@
+package Q151TO200.number165;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/9/2 17:16
+ * @description: 字符串比较
+ */
+public class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        for (int i = 0; i < v1.length || i < v2.length; ++i) {
+            int x = 0, y = 0;
+            if (i < v1.length) {
+                x = Integer.parseInt(v1[i]);
+            }
+            if (i < v2.length) {
+                y = Integer.parseInt(v2[i]);
+            }
+            if (x > y) {
+                return 1;
+            }
+            if (x < y) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+}
