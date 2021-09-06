1b4d58b39f624f9b9dd0d00373d005743d860639
Bright Chan <980231201@qq.com>
2021-09-06 12:57:44 +0800

    No.704 solved.

---
+++ src/Q701TO750/number704/Solution.java
@@ -0,0 +1,24 @@
+package Q701TO750.number704;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/9/6 12:57
+ * @description: 二分查找
+ */
+public class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
