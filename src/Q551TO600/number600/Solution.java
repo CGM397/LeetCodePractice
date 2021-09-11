342318f4c715f0c7e080ad83246958e87b339da8
Bright Chan <980231201@qq.com>
2021-09-11 20:14:18 +0800

    No.600 solved.

---
+++ src/Q551TO600/number600/Solution.java
@@ -0,0 +1,36 @@
+package Q551TO600.number600;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/9/11 20:13
+ * @description: dp
+ */
+public class Solution {
+    public int findIntegers(int n) {
+        int[] dp = new int[31];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i < 31; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        int pre = 0, res = 0;
+        for (int i = 29; i >= 0; --i) {
+            int val = 1 << i;
+            if ((n & val) != 0) {
+                res += dp[i + 1];
+                if (pre == 1) {
+                    break;
+                }
+                pre = 1;
+            } else {
+                pre = 0;
+            }
+
+            if (i == 0) {
+                ++res;
+            }
+        }
+
+        return res;
+    }
+}
