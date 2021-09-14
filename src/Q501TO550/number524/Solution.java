be269573611e6739c7ca396ddead1d455be84cd2
Bright Chan <980231201@qq.com>
2021-09-14 15:55:11 +0800

    No.524 solved.

---
+++ src/Q501TO550/number524/Solution.java
@@ -0,0 +1,29 @@
+package Q501TO550.number524;
+
+import java.util.List;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/9/14 15:50
+ * @description: TODO
+ */
+public class Solution {
+    public String findLongestWord(String s, List<String> dictionary) {
+        String res = "";
+        for (String t : dictionary) {
+            int i = 0, j = 0;
+            while (i < t.length() && j < s.length()) {
+                if (t.charAt(i) == s.charAt(j)) {
+                    ++i;
+                }
+                ++j;
+            }
+            if (i == t.length()) {
+                if (t.length() > res.length() || (t.length() == res.length() && t.compareTo(res) < 0)) {
+                    res = t;
+                }
+            }
+        }
+        return res;
+    }
+}
