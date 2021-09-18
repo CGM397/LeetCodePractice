83eaeee6135c2643f9e9519be89f1c669a040144
Bright Chan <980231201@qq.com>
2021-09-18 18:52:27 +0800

    No.292 solved.

---
+++ src/Q251TO300/number292/Solution.java
@@ -0,0 +1,12 @@
+package Q251TO300.number292;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/9/18 18:44
+ * @description: 数学
+ */
+public class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
