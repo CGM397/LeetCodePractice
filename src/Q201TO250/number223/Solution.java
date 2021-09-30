c501507769fc3521d23b643a3f77dfae56a7c9c2
Bright Chan <980231201@qq.com>
2021-09-30 12:13:44 +0800

    No.223 solved.

---
+++ src/Q201TO250/number223/Solution.java
@@ -0,0 +1,15 @@
+package Q201TO250.number223;
+
+/**
+ * @author: Bright Chan
+ * @date: 2021/9/30 12:12
+ * @description: 数学
+ */
+public class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1), area2 = (bx2 - bx1) * (by2 - by1);
+        int overlapWidth = Math.min(ax2, bx2) - Math.max(ax1, bx1), overlapHeight = Math.min(ay2, by2) - Math.max(ay1, by1);
+        int overlapArea = Math.max(overlapWidth, 0) * Math.max(overlapHeight, 0);
+        return area1 + area2 - overlapArea;
+    }
+}
