f7a6aa7448669e90acf0296a188b4f0f5f940dd6
CGM397 <980231201@qq.com>
2023-03-03 23:17:40 +0800

    No.1487 solved

---
+++ src/Q1451TO1500/number1487/Solution.java
@@ -0,0 +1,37 @@
+package Q1451TO1500.number1487;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: Solution
+ * @date: 2023/3/3 23:08
+ * @description: getFolderNames
+ */
+public class Solution {
+    public String[] getFolderNames(String[] names) {
+        Map<String, Integer> index = new HashMap<String, Integer>();
+        int n = names.length;
+        String[] res = new String[n];
+        for (int i = 0; i < n; i++) {
+            String name = names[i];
+            if (!index.containsKey(name)) {
+                res[i] = name;
+                index.put(name, 1);
+            } else {
+                int k = index.get(name);
+                while (index.containsKey(addSuffix(name, k))) {
+                    k++;
+                }
+                res[i] = addSuffix(name, k);
+                index.put(name, k + 1);
+                index.put(addSuffix(name, k), 1);
+            }
+        }
+        return res;
+    }
+
+    public String addSuffix(String name, int k) {
+        return name + "(" + k + ")";
+    }
+}
