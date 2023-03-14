e1e21668fbb98c3119108599ea6387ad1ed41a22
CGM397 <980231201@qq.com>
2023-03-14 22:30:16 +0800

    No.1603 solved

---
+++ src/Q1601TO1650/number1605/Solution.java
@@ -0,0 +1,27 @@
+package Q1601TO1650.number1605;
+
+/**
+ * @author: Solution
+ * @date: 2023/3/14 21:43
+ * @description: restoreMatrix
+ */
+public class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int n = rowSum.length, m = colSum.length;
+        int[][] matrix = new int[n][m];
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            int v = Math.min(rowSum[i], colSum[j]);
+            matrix[i][j] = v;
+            rowSum[i] -= v;
+            colSum[j] -= v;
+            if (rowSum[i] == 0) {
+                ++i;
+            }
+            if (colSum[j] == 0) {
+                ++j;
+            }
+        }
+        return matrix;
+    }
+}
