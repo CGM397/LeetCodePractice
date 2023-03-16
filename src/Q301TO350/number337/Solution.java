a3e5a79bd05108091915e4f0ed10b11f4d519391
CGM397 <980231201@qq.com>
2023-03-16 21:44:31 +0800

    No.337 solved

---
+++ src/Q301TO350/number337/Solution.java
@@ -0,0 +1,42 @@
+package Q301TO350.number337;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: Solution
+ * @date: 2023/3/16 21:35
+ * @description: rob
+ */
+public class Solution {
+    Map<TreeNode, Integer> f = new HashMap<>();
+    Map<TreeNode, Integer> g = new HashMap<>();
+
+    public int rob(TreeNode root) {
+        dfs(root);
+        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+    }
+
+    public void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.left);
+        dfs(node.right);
+        f.put(node, node.val + g.getOrDefault(node.left, 0) + g.getOrDefault(node.right, 0));
+        g.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
