1feeff0f6000873cc7b1a63c7c30bbb3e52a91c2
CGM397 <980231201@qq.com>
2023-03-21 23:25:25 +0800

    No.2469 solved

---
+++ src/Q2451TO2500/number2469/Solution.java
@@ -0,0 +1,12 @@
+package Q2451TO2500.number2469;
+
+/**
+ * @author: Solution
+ * @date: 2023/3/21 23:21
+ * @description: convertTemperature
+ */
+public class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32.00};
+    }
+}
